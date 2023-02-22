dd698c062695e58c34f7fe1d971616ab23a68d1e
Kevin Herbst <kevin-herbst1993@web.de>
2023-02-22 01:27:10 +0100

    added images slideShow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'KevinHerbst';
+export class AppComponent implements OnInit {
+  images = ['DSC00333.JPG', 'DSC00334.JPG', 'TinderBild2.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+   updateImage() {
+    setInterval( () => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+
 }
